2c9878109e6530789ee93f94da864ca42629c848
MikeYaye <m.moolenaar99@gmail.com>
2018-02-09 16:53:05 +0100

    Small format change in C++ wrapper

--- AnitomyNET_Wrapper/anitomy_wrapper.h
+++ AnitomyNET_Wrapper/anitomy_wrapper.h
@@ -6,11 +6,11 @@
 
 std::string convert_to_string(anitomy::string_t input_str);
 LPSAFEARRAY convert_to_safe_array(std::vector<std::string> input_array);
-extern "C" __declspec(dllexport) LPSAFEARRAY parse_anime_file_name(const wchar_t*	file_name,
-																   const wchar_t*	ignored_strings[],
-															       const int		ignored_strings_length,
-																   const wchar_t*	allowed_delimiters,
-																   const bool		parse_episode_number,
-																   const bool		parse_episode_title,
-																   const bool		parse_file_extension,
-																   const bool		parse_release_group);
+extern "C" __declspec(dllexport) LPSAFEARRAY parse_anime_file_name(const wchar_t* file_name,
+                                                                   const wchar_t* ignored_strings[],
+                                                                   const int ignored_strings_length,
+                                                                   const wchar_t* allowed_delimiters,
+                                                                   const bool parse_episode_number,
+                                                                   const bool parse_episode_title,
+                                                                   const bool parse_file_extension,
+                                                                   const bool parse_release_group);
