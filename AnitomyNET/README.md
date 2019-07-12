1bed362154e6e3146c7eee4aca5eb5722d94ed91
MikeYaye <m.moolenaar99@gmail.com>
2019-07-12 16:14:48 +0200

    Added better support for x64 platforms and x86 running on x64

---
+++ AnitomyNET/README.md
@@ -0,0 +1,16 @@
+# How to build
+## Making changes to the wrapper
+When done making changes to the wrapper, build the AnitomyNET_Wrapper project in x64 and x86 Release platform configurations. This will place them in the right folder.
+
+## Building for NuGET
+- Don't forget to change the version number via Properties => Package.
+- Build the AnitomyNEt project in Release mode, this will make a NuGET package file.
+- After you built the `.nupkg` package file, the `AnitomyNET_Wrapper_64.dll` and `AnitomyNET_Wrapper_86.dll` wrapper files are not included yet. 
+- Open the `.nupkg` file in [NuGet Package Explorer](https://github.com/NuGetPackageExplorer/NuGetPackageExplorer):
+- right mouse click -> `Add Build Folder`
+- Place the 2 wrapper files and the `Anitomy.NET.props` (you can find them in the `netstandard1.*` folder) in the build folder.
+It should look like this:
+![Package contents screenshot](https://raw.githubusercontent.com/MikeYaye/anitomy.NET/CorrectPackageContentNuGet.png)
+
+You should test it in a seperate project, install the NuGET package from the file with `Install-Package SomePackage -Source "path/to/nupkg-file"` in the Package Manager Console.  
+After that, the package file is ready to upload to NuGET.
--- AnitomyNET/anitomyNET.cs
+++ AnitomyNET/anitomyNET.cs
@@ -1,35 +1,60 @@
-﻿/* This Source Code Form is subject to the terms of the Mozilla Public
+/* This Source Code Form is subject to the terms of the Mozilla Public
   License, v. 2.0. See the LICENCE file for more info.*/
 
+using System;
+using System.IO;
 using System.Runtime.InteropServices;
+using System.Reflection;
 
 namespace AnitomyNET
 {
 
     public class Anitomy
     {
-        [DllImport("AnitomyNET_Wrapper.dll", CallingConvention = CallingConvention.Cdecl, CharSet = CharSet.Unicode)]
-        [return: MarshalAs(UnmanagedType.SafeArray)]
-        private static extern string[] parse_anime_file_name(string fileName, string[] ignoredStrings, int ignoredStringsLength, string allowedDelimiters, bool parseEpisodeNumber, bool parseEpisodeTitle, bool parseFileExtension, bool parseReleaseGroup);
+        private static bool platformIs64 = IntPtr.Size == 8;
+
+        private static class Platform64
+        {
+            [DllImport("AnitomyNET_Wrapper_64.dll", CallingConvention = CallingConvention.Cdecl, CharSet = CharSet.Unicode)]
+            [return: MarshalAs(UnmanagedType.SafeArray)]
+            public static extern string[] parse_anime_file_name(string fileName, string[] ignoredStrings, int ignoredStringsLength, string allowedDelimiters, bool parseEpisodeNumber, bool parseEpisodeTitle, bool parseFileExtension, bool parseReleaseGroup);
+        }
+
+        private static class Platform86
+        {
+            [DllImport("AnitomyNET_Wrapper_86.dll", CallingConvention = CallingConvention.Cdecl, CharSet = CharSet.Unicode)]
+            [return: MarshalAs(UnmanagedType.SafeArray)]
+            public static extern string[] parse_anime_file_name(string fileName, string[] ignoredStrings, int ignoredStringsLength, string allowedDelimiters, bool parseEpisodeNumber, bool parseEpisodeTitle, bool parseFileExtension, bool parseReleaseGroup);
+        }
+
 
         /// <summary>
         /// Parses anime file name into an AnimeFile object
         /// </summary>
         public static AnimeFile Parse(string fileName, AnitomyOptions options = null)
         {
-            if (options == null)
-                options = new AnitomyOptions();
+            if (options == null) options = new AnitomyOptions();
 
-            string[] parsedResult = parse_anime_file_name(
-                fileName,
-                options.ignoredStrings ?? new string[0],
-                options.ignoredStrings?.Length ?? 0,
-                options.allowedDelimiters,
-                options.parseEpisodeNumber,
-                options.parseEpisodeTitle,
-                options.parseFileExtension,
-                options.parseReleaseGroup
-             );
+            string[] parsedResult = platformIs64 ? Platform64.parse_anime_file_name(
+                    fileName,
+                    options.ignoredStrings ?? new string[0],
+                    options.ignoredStrings?.Length ?? 0,
+                    options.allowedDelimiters,
+                    options.parseEpisodeNumber,
+                    options.parseEpisodeTitle,
+                    options.parseFileExtension,
+                    options.parseReleaseGroup
+                )
+                : Platform86.parse_anime_file_name(
+                    fileName,
+                    options.ignoredStrings ?? new string[0],
+                    options.ignoredStrings?.Length ?? 0,
+                    options.allowedDelimiters,
+                    options.parseEpisodeNumber,
+                    options.parseEpisodeTitle,
+                    options.parseFileExtension,
+                    options.parseReleaseGroup
+                );
             return new AnimeFile(parsedResult);
         }
     }
